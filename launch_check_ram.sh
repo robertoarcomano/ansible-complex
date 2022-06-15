495cc8d4236f1b7a5e5e626dce361fd6b474d95c
Roberto Arcomano <bertolinux@gmail.com>
2022-06-15 16:53:58 +0200

    - copy instead of ansible.builtin.copy - removed verbose mode

--- launch_check_ram.sh
+++ launch_check_ram.sh
@@ -7,6 +7,7 @@ CHECK=""
 INVENTORY="inventory.yml"
 PLAYBOOK="check_ram.yml"
 VERBOSE="-v"
+VERBOSE=""
 
 # 1. Launch ansible
-ansible-playbook -i $INVENTORY $PLAYBOOK $CHECK "$VERBOSE" --limit $LIMIT
+ansible-playbook -i $INVENTORY $PLAYBOOK $CHECK $VERBOSE --limit $LIMIT
