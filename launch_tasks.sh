4f9d980c0b5070294d800e36ff56ddfb28086b95
Roberto Arcomano <bertolinux@gmail.com>
2022-06-24 17:29:35 +0200

    Added roles

--- launch_tasks.sh
+++ launch_tasks.sh
@@ -7,7 +7,7 @@ CHECK=""
 INVENTORY="inventory.yml"
 PLAYBOOK="tasks.yml"
 VERBOSE="-v"
-VERBOSE=""
+#VERBOSE=""
 
 # 1. Launch ansible
 ansible-playbook -i $INVENTORY $PLAYBOOK $CHECK $VERBOSE --limit $LIMIT --ask-vault-pass
